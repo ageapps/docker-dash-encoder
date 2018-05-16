9fbc61d6e82354f2353e7f9cafe23218bec38acb
Adrián García Espinosa <age.apps.dev@gmail.com>
2018-05-16 20:05:21 +0200

    updt: arch diagram, convert options, packager using variables

--- convert.sh
+++ convert.sh
@@ -1,12 +1,29 @@
 #!/bin/bash          
 
-inputpath=$1
+inputpath=""
+option=""
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+if [ $# -eq 2 ]
+  then
+    option=$1
+    inputpath=$2
+  else
+    inputpath=$1
+fi
 inputfile=$(basename "$inputpath")
 filename=${inputfile%.*}
 outdir="/media/out/_$filename"
-QUALITIES=( "360" "480" "720" "1080" )
-RATES=( "600" "1000" "3000" "6000" )
-LEVELS=( "3.0" "3.1" "4.0" "4.2" )
+
+# QUALITIES=( "240" "360" "480" "720" "1080")
+# RATES=( "240" "600" "1000" "3000" "6000")
+# LEVELS=( "3.0" "3.0" "3.1" "4.0" "4.1")
+QUALITIES=( "120" "240" "360" "480" )
+RATES=( "100" "240" "600" "1000" )
+LEVELS=( "3.0" "3.0" "3.0" "3.1" )
 
 mp4streams=""
 
@@ -24,14 +41,17 @@ do
     echo "Enconding $filename into MPEG4 Q:$quality R:$rate L:$level"
     outfile="$outdir/h264_baseline_${quality}p_${rate}.mp4"
     mp4streams+=" $outfile#video:id=h264_${quality}" 
-    ffmpeg -i $inputpath -c:a copy \
-        -vf "scale=-2:$quality" \
-        -c:v libx264 -profile:v baseline -level:v $level \
-        -x264opts scenecut=0:open_gop=0:min-keyint=72:keyint=72 \
-        -minrate ${rate}k -maxrate ${rate}k -bufsize ${rate}k -b:v ${rate}k \
-        -y $outfile
+    if [ -z "$option" ]
+      then
+        ffmpeg -i $inputpath -c:a copy \
+            -vf "scale=-2:$quality" \
+            -c:v libx264 -profile:v baseline -level:v $level \
+            -x264opts scenecut=0:open_gop=0:min-keyint=72:keyint=72 \
+            -minrate ${rate}k -maxrate ${rate}k -bufsize ${rate}k -b:v ${rate}k \
+            -y $outfile
+    fi
 done
-    MP4Box -dash 4000 -rap -bs-switching no -profile live -segment-ext null -out $outdir/manifest.mpd "$outdir/h264_baseline_${QUALITIES[0]}p_${RATES[0]}.mp4#audio" $mp4streams
+    MP4Box -dash 4000 -rap -bs-switching no -profile live -out $outdir/manifest.mpd "$outdir/h264_baseline_${QUALITIES[0]}p_${RATES[0]}.mp4#audio" $mp4streams
 
 
 
--- shaka-packager/convert-shaka.sh
+++ shaka-packager/convert-shaka.sh
@@ -1,12 +1,29 @@
 #!/bin/bash          
 
-inputpath=$1
+inputpath=""
+option=""
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+if [ $# -eq 2 ]
+  then
+    option=$1
+    inputpath=$2
+  else
+    inputpath=$1
+fi
 inputfile=$(basename "$inputpath")
 filename=${inputfile%.*}
 outdir="/media/out/$filename"
-QUALITIES=( "360" "480" "720" "1080" )
-RATES=( "600" "1000" "3000" "6000" )
-LEVELS=( "3.0" "3.1" "4.0" "4.2" )
+
+# QUALITIES=( "240" "360" "480" "720" "1080")
+# RATES=( "240" "600" "1000" "3000" "6000")
+# LEVELS=( "3.0" "3.0" "3.1" "4.0" "4.1")
+QUALITIES=( "120" "240" "360" "480" )
+RATES=( "100" "240" "600" "1000" )
+LEVELS=( "3.0" "3.0" "3.0" "3.1" )
 
 mp4streams=""
 
@@ -18,24 +35,25 @@ do
     rate=${RATES[$i]}
     level=${LEVELS[$i]}
 
-    # outdir="./out/${srt_dim}_$filename"
     echo "Creating folder for $filename"
     mkdir -p $outdir
     echo "Enconding $filename into MPEG4 Q:$quality R:$rate L:$level"
     outfile="$outdir/h264_baseline_${quality}p_${rate}.mp4"
-    mp4streams+=" $outfile#video" 
-    ffmpeg -i $inputpath -c:a copy \
-        -vf "scale=-2:$quality" \
-        -c:v libx264 -profile:v baseline -level:v $level \
-        -x264opts scenecut=0:open_gop=0:min-keyint=72:keyint=72 \
-        -minrate ${rate}k -maxrate ${rate}k -bufsize ${rate}k -b:v ${rate}k \
-        -y $outfile
+    mp4streams+=" in=h264_baseline_${quality}p_${rate}.mp4,stream=video,init_segment=h264_${quality}p/init.mp4,segment_template=h264_${quality}p/\$Number\$.m4s"
+    if [ -z "$option" ]
+      then 
+        ffmpeg -i $inputpath -c:a copy \
+            -vf "scale=-2:$quality" \
+            -c:v libx264 -profile:v baseline -level:v $level \
+            -x264opts scenecut=0:open_gop=0:min-keyint=72:keyint=72 \
+            -minrate ${rate}k -maxrate ${rate}k -bufsize ${rate}k -b:v ${rate}k \
+            -y $outfile
+    fi
 done
+
+    audioInput="in=h264_baseline_${QUALITIES[0]}p_${RATES[0]}.mp4,stream=audio,init_segment=audio/init.mp4,segment_template=audio/\$Number\$.m4s"
     cd $outdir
     packager \
-        'in=h264_baseline_360p_600.mp4,stream=audio,init_segment=audio/init.mp4,segment_template=audio/$Number$.m4s' \
-        'in=h264_baseline_360p_600.mp4,stream=video,init_segment=h264_360p/init.mp4,segment_template=h264_360p/$Number$.m4s' \
-        'in=h264_baseline_480p_1000.mp4,stream=video,init_segment=h264_480p/init.mp4,segment_template=h264_480p/$Number$.m4s' \
-        'in=h264_baseline_720p_3000.mp4,stream=video,init_segment=h264_720p/init.mp4,segment_template=h264_720p/$Number$.m4s' \
-        'in=h264_baseline_1080p_6000.mp4,stream=video,init_segment=h264_1080p/init.mp4,segment_template=h264_1080p/$Number$.m4s' \
+        ${audioInput} \
+        ${mp4streams} \
         --generate_static_mpd --mpd_output h264.mpd
